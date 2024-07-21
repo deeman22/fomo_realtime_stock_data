9107e488621a9fdc5c9a2918d299d02f4b46765b
Bunny <biswajitdas@Biswajits-MacBook-Air.local>
2024-07-21 16:09:29 +0530

    Backend project structure added

---
+++ RealTimePriceDataBackend/src/main/java/com/fomo/RealTimePriceDataBackend/RealTimePriceDataBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fomo.RealTimePriceDataBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimePriceDataBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimePriceDataBackendApplication.class, args);
+	}
+
+}
---
+++ RealTimePriceDataBackend/src/test/java/com/fomo/RealTimePriceDataBackend/RealTimePriceDataBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fomo.RealTimePriceDataBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimePriceDataBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
